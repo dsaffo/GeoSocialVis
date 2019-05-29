9aae04bdb00811e3173ec27f86dd8312eedc3f61
michaschwab <me@michaschwab.de>
2019-05-29 14:53:24 -0400

    separate out the network visualization aspect to keep the main js file lean

---
+++ network-vis.js
@@ -0,0 +1,125 @@
+const NetworkVis = (function(projection) {
+    const svg = d3.select("svg"),
+        width = +svg.attr("width"),
+        height = +svg.attr("height");
+
+    const forceBalance = ForceBalance(projection);
+    const simulation = forceBalance.simulation;
+
+    var path = d3.geoPath().projection(projection);
+
+    const affiliationsWithLocation = affiliations
+        .filter(a => a.Position);
+    for(const affiliation of affiliationsWithLocation) {
+        const splitPos = affiliation.Position.split(',');
+        affiliation.lat = parseFloat(splitPos[0]);
+        affiliation.lng = parseFloat(splitPos[1]);
+    }
+    const affiliationsByName = {};
+    for(const affiliation of affiliations) {
+        affiliationsByName[affiliation.Name] = affiliation;
+    }
+
+    svg.append('g')
+        .attr('class', 'affiliations')
+        .selectAll('circle')
+        .data(affiliationsWithLocation)
+        .enter().append('circle');
+
+    var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+    for(const node of graph.nodes) {
+        const match = affiliationsWithLocation.filter(a => a.Name === node.affiliation);
+        if(match && match.length > 0) {
+            node.lat = match[0].lat;
+            node.lng = match[0].lng;
+            node.x = projection([node.lng, node.lat])[0];
+            node.y = projection([node.lng, node.lat])[1];
+        }
+    }
+
+    graph.nodes.sort((a,b) => a.paperIndex.length - b.paperIndex.length);
+
+    var link = svg.append("g")
+        .attr("class", "links")
+        .selectAll("line")
+        .data(graph.links)
+        .enter().append("line")
+        .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+    var nodesG = svg.append("g")
+        .attr("class", "nodes");
+
+    var node = nodesG
+        .selectAll("circle")
+        .data(graph.nodes)
+        .enter().append("circle")
+        .attr("r", d => Math.sqrt(20 * d.paperIndex.length))
+        .attr("fill", function(d) {
+            const affiliation = affiliationsByName[d.affiliation];
+            if(!affiliation) {
+                return '#aaa';
+            }
+            const name = affiliation.NameSimple ? affiliation.NameSimple : d.affiliation;
+            return color(name);
+        })
+        .call(d3.drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended));
+
+    graph.nodes.forEach(d => {
+        if(d.paperIndex.length >= 8) {
+            d.textEl = svg.append('text')
+                .attr('text-anchor', 'middle')
+                .attr('dy', 5)
+                .attr('font-size', 16)
+                .attr('pointer-events', 'none')
+                .text(() => {
+                    const nameParts = d.id.split(' ');
+                    const initials = nameParts[0][0] + nameParts[nameParts.length - 1][0];
+                    return d.paperIndex.length >= 8 ? initials : '';
+                });
+        }
+    });
+
+    simulation
+        .nodes(graph.nodes)
+        .on("tick", ticked);
+    simulation.force("link")
+        .links(graph.links);
+
+    function ticked() {
+        link
+            .attr("x1", function(d) { return d.source.x; })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; });
+
+        nodesG.selectAll('circle')
+            .attr('cx', d => d.x)
+            .attr('cy', d => d.y);
+
+        graph.nodes.forEach(d => {
+            if(d.textEl) {
+                d.textEl
+                    .attr('x', d.x)
+                    .attr('y', d.y);
+            }
+        });
+    }
+    function dragstarted(d) {
+        if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+        d.fx = d.x;
+        d.fy = d.y;
+    }
+    function dragged(d) {
+        d.fx = d3.event.x;
+        d.fy = d3.event.y;
+    }
+    function dragended(d) {
+        if (!d3.event.active) simulation.alphaTarget(0);
+        d.fx = null;
+        d.fy = null;
+    }
+});
--- panel.js
+++ panel.js
@@ -1,3 +1,5 @@
 (function() {
+    const element = document.getElementsByClassName('panel')[0];
+
 
 })();
