470440e6bbf55606d1b9ed2a6853c8c9785f48a2
michaschwab <me@michaschwab.de>
2019-05-29 14:41:48 -0400

    use some closures to standardize communication

--- force-balance.js
+++ force-balance.js
@@ -1,69 +1,78 @@
-var svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
+const ForceBalance = (function(projection){
+    var svg = d3.select("svg"),
+        width = +svg.attr("width"),
+        height = +svg.attr("height");
 
-const forceMapStrengths = {
-    link: 0,
-    charge: 0,
-    radial: 0,
-    geoPosition: 1,
-    mapOpacity: 1,
-    center: 0
-};
+    const geoPositionForce = forceDefinitions.geoPositionForce;
+    const centeringForce = forceDefinitions.centeringForce;
 
-const forceAuthorshipStrengths = {
-    link: 0.6,
-    charge: -30,
-    radial: 0,//0.07,
-    geoPosition: 0,
-    mapOpacity: 0,
-    center: 0.015
-};
+    const forceMapStrengths = {
+        link: 0,
+        charge: 0,
+        radial: 0,
+        geoPosition: 1,
+        mapOpacity: 1,
+        center: 0
+    };
 
-function forceSelection(mapPortion){
-    return {
-        link: forceMapStrengths.link * mapPortion + forceAuthorshipStrengths.link * (1 - mapPortion),
-        charge: forceMapStrengths.charge * mapPortion + forceAuthorshipStrengths.charge * (1 - mapPortion),
-        radial: forceMapStrengths.radial * mapPortion + forceAuthorshipStrengths.radial * (1 - mapPortion),
-        geoPosition: forceMapStrengths.geoPosition * mapPortion + forceAuthorshipStrengths.geoPosition * (1 - mapPortion),
-        mapOpacity: forceMapStrengths.mapOpacity * mapPortion + forceAuthorshipStrengths.mapOpacity * (1 - mapPortion),
-        center: forceMapStrengths.center * mapPortion + forceAuthorshipStrengths.center * (1 - mapPortion),
+    const forceAuthorshipStrengths = {
+        link: 0.6,
+        charge: -30,
+        radial: 0,//0.07,
+        geoPosition: 0,
+        mapOpacity: 0,
+        center: 0.015
+    };
+
+    function forceSelection(mapPortion){
+        return {
+            link: forceMapStrengths.link * mapPortion + forceAuthorshipStrengths.link * (1 - mapPortion),
+            charge: forceMapStrengths.charge * mapPortion + forceAuthorshipStrengths.charge * (1 - mapPortion),
+            radial: forceMapStrengths.radial * mapPortion + forceAuthorshipStrengths.radial * (1 - mapPortion),
+            geoPosition: forceMapStrengths.geoPosition * mapPortion + forceAuthorshipStrengths.geoPosition * (1 - mapPortion),
+            mapOpacity: forceMapStrengths.mapOpacity * mapPortion + forceAuthorshipStrengths.mapOpacity * (1 - mapPortion),
+            center: forceMapStrengths.center * mapPortion + forceAuthorshipStrengths.center * (1 - mapPortion),
+        }
     }
-}
 
-var slider = document.getElementById("myRange");
-var forceSettings = forceSelection(slider.value);
-let previousSliderValue = slider.value;
+    var slider = document.getElementById("myRange");
+    var forceSettings = forceSelection(slider.value);
+    let previousSliderValue = slider.value;
 
-slider.oninput = function() {
-    const sliderValue = this.value;
-    const difference = Math.abs(previousSliderValue - sliderValue);
-    const newAlpha = simulation.alpha()+difference;
-    previousSliderValue = sliderValue;
-    simulation.alpha(newAlpha).restart();
-    forceSettings = forceSelection(this.value);
+    slider.oninput = function() {
+        const sliderValue = this.value;
+        const difference = Math.abs(previousSliderValue - sliderValue);
+        const newAlpha = simulation.alpha()+difference;
+        previousSliderValue = sliderValue;
+        simulation.alpha(newAlpha).restart();
+        forceSettings = forceSelection(this.value);
+        applyStrengths(forceSettings);
+
+        document.getElementById('map-background').style.opacity = forceSettings.mapOpacity;
+    };
+
+    var simulation = d3.forceSimulation().alphaDecay(0.02);
+
+    const forceLink = d3.forceLink().id(function(d) { return d.id; });
+    const forceManyBody = d3.forceManyBody();
+    const forceCollide = d3.forceCollide().radius(function(d) {
+        return Math.sqrt(30 * d.paperIndex.length)
+    });
+    const forceRadial = d3.forceRadial(300, width / 2, height / 2);
+    const forceGeo = geoPositionForce(projection);
+    const forceCenter = centeringForce(width / 2, height / 2);
+
+    function applyStrengths(strength) {
+        simulation.force("link", forceLink.strength(strength.link))
+            .force("charge", forceManyBody.strength(strength.charge))
+            .force('collision', forceCollide)
+            .force("radial", forceRadial.strength(strength.radial))
+            .force("center", forceCenter.strength(strength.center))
+            .force('geoposition', forceGeo.strength(strength.geoPosition));
+    }
     applyStrengths(forceSettings);
 
-    document.getElementById('map-background').style.opacity = forceSettings.mapOpacity;
-};
-
-var simulation = d3.forceSimulation().alphaDecay(0.02);
-
-const forceLink = d3.forceLink().id(function(d) { return d.id; });
-const forceManyBody = d3.forceManyBody();
-const forceCollide = d3.forceCollide().radius(function(d) {
-    return Math.sqrt(30 * d.paperIndex.length)
+    return {
+        simulation
+    };
 });
-const forceRadial = d3.forceRadial(300, width / 2, height / 2);
-const forceGeo = geoPositionForce();
-const forceCenter = centeringForce(width / 2, height / 2);
-
-function applyStrengths(strength) {
-    simulation.force("link", forceLink.strength(strength.link))
-        .force("charge", forceManyBody.strength(strength.charge))
-        .force('collision', forceCollide)
-        .force("radial", forceRadial.strength(strength.radial))
-        .force("center", forceCenter.strength(strength.center))
-        .force('geoposition', forceGeo.strength(strength.geoPosition));
-}
-applyStrengths(forceSettings);
